3e015e47cf71d3bfad9aab0d4e27b834366652d5
ij-25 <128509515+ij-25@users.noreply.github.com>
2023-07-19 19:23:12 +0530

    second commit

--- live.js
+++ live.js
@@ -1,36 +1,77 @@
-const balance = document.getElementById("balance");
-const money_plus = document.getElementById("money-plus");
-const money_minus = document.getElementById("money-minus");
-const list = document.getElementById("list");
-const form = document.getElementById("form");
-const text = document.getElementById("text");
-const amount = document.getElementById("amount");
-const addbtn = document.getElementById("add");
-let Tr = [];
-addbtn.addEventListener("click" , addTr )
-function addTr () {
-Tr.push({
-  amount,text
-})
-console.log( "hi" , Tr);
+const balanceElement = document.getElementById("balance");
+const incomeElement = document.getElementById("money-plus");
+const expenseElement = document.getElementById("money-minus");
+const historyList = document.getElementById("history-list");
+const textInput = document.getElementById("text");
+const amountInput = document.getElementById("amount");
+const addBtn = document.getElementById("add");
+
+let balance = 0;
+let income = 0;
+let expense = 0;
+
+function updateBalance() {
+  balance = income - expense;
+  balanceElement.innerText = `₹ ${balance.toFixed(2)}`;
+  incomeElement.innerText = `+ ₹ ${income.toFixed(2)}`;
+  expenseElement.innerText = `- ₹ ${expense.toFixed(2)}`;
 }
-const dummyTransactions = [
-  { id: 1, text: "flower", amount: -30 },
-  { id: 2, text: "Chai", amount: -10 },
-  { id: 3, text: "Toast", amount: -15 },
-  { id: 4, text: "Brownie", amount: -45 },
-];
-let transactions = dummyTransactions;
-function addTransactionDOM(transaction){
-    console.log(transaction);
-    const sign = transaction[0].amount < 0 ?" - " : "+";
-    const item = document.createElement("li");
-    item.classList.add(
-        transaction[0].amount < 0 ? "minus" : "plus"
-    )
-    item.innerHTML = `
-    ${transaction[0].text}<span>${sign}${Math.abs(transaction[0].amount)}</span>
-    <button class= "delete-btn" onclick= "" >`;
-    list.appendChild(item);
+
+function updateIncomeAndExpense(amount) {
+  if (amount >= 0) {
+    income += amount;
+  } else {
+    expense -= amount;
+  }
+
+  updateBalance();
 }
-addTransactionDOM(transactions);
+
+function addTransaction(text, amount) {
+  const listItem = document.createElement("li");
+  listItem.className =
+    amount < 0
+      ? "flex text-xl bg-slate-200 justify-items-end pt-2 minus"
+      : "flex text-xl bg-slate-200 justify-items-end pt-2 plus";
+
+  listItem.innerHTML = `
+    ${text} <span> ₹ ${amount.toFixed(
+    2
+  )} </span><button class="delete-btn">X</button>
+  `;
+
+  historyList.appendChild(listItem);
+
+  updateIncomeAndExpense(amount);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const text = textInput.value.trim();
+  let amount = parseFloat(amountInput.value);
+
+  if (text.includes("+")) {
+    const parts = text.split("+");
+    if (parts.length === 2) {
+      const description = parts[0].trim();
+      const positiveAmount = parseFloat(parts[1].trim());
+      if (!isNaN(positiveAmount)) {
+        amount = positiveAmount;
+        textInput.value = description;
+      }
+    }
+  }
+
+  if (text === "" || isNaN(amount) || amount === 0) {
+    alert("Please enter valid text and amount.");
+    return;
+  }
+
+  addTransaction(text, amount);
+
+  textInput.value = "";
+  amountInput.value = "";
+}
+
+addBtn.addEventListener("click", handleSubmit);
